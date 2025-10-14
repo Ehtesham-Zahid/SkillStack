338570c47040dd89595b7c7c5c2af34b447bcbfa
Ehtesham-Zahid <shamimalick321@gmail.com>
2025-10-14 14:55:26 +0500

    User Api Looks Good

--- client/src/redux/features/api/apiSlice.ts
+++ client/src/redux/features/api/apiSlice.ts
@@ -4,7 +4,7 @@ import { userLoggedIn } from "../auth/authSlice";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_SERVER_URL }),
-  tagTypes: ["Users", "Courses", "Layout", "Course"],
+  tagTypes: ["Users", "Courses", "Layout", "Course", "Orders"],
   endpoints: (builder) => ({
     // refreshToken: builder.query({
     //   query: () => ({
