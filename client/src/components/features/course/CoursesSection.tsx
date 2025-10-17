fefaa93841b132b41e35746c9d5cda12fa682921
Ehtesham-Zahid <shamimalick321@gmail.com>
2025-10-17 12:11:43 +0500

    Courses Section Updated

--- client/src/components/features/course/CoursesSection.tsx
+++ client/src/components/features/course/CoursesSection.tsx
@@ -136,7 +136,7 @@ const CoursesSection = () => {
           </div>
 
           {/* Courses Grid */}
-          <div className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 px-4   mx-auto">
+          <div className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-8 px-4   mx-auto">
             {filteredCourses.length > 0 ? (
               filteredCourses.map((course: any) => (
                 <CourseCard
