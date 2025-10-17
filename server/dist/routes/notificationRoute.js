983740b78e6532bbd93c0ca56b9a9cb12b376926
Ehtesham-Zahid <shamimalick321@gmail.com>
2025-10-18 00:00:26 +0500

    Cleaning Code

--- server/dist/routes/notificationRoute.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { handleGetAllNotifications, handleUpdateNotificationStatus, } from "../controllers/notificationController";
-import { authorizeRoles, isAuthenticated } from "../middleware/auth";
-import { handleUpdateAccessToken } from "../controllers/userController";
-const notificationRouter = express.Router();
-notificationRouter.get("/get-all-notifications", handleUpdateAccessToken, isAuthenticated, authorizeRoles("admin"), handleGetAllNotifications);
-notificationRouter.put("/update-notification-status/:id", handleUpdateAccessToken, isAuthenticated, authorizeRoles("admin"), handleUpdateNotificationStatus);
-export default notificationRouter;
