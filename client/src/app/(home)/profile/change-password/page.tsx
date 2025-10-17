9e95715e58aa62620110278cbfb35237263c7c4e
Ehtesham-Zahid <shamimalick321@gmail.com>
2025-10-17 21:09:37 +0500

    Protected Routes added

--- client/src/app/(home)/profile/change-password/page.tsx
+++ client/src/app/(home)/profile/change-password/page.tsx
@@ -1,7 +1,25 @@
-import React from "react";
-import ChangePassword from "@/src/components/features/profile/ChangePassword";
+"use client";
 
-const page = () => {
+import React, { useEffect } from "react";
+import ChangePassword from "@/src/components/features/profile/ChangePassword";
+import { useLoadUserQuery } from "@/src/redux/features/api/apiSlice";
+import { useRouter } from "next/navigation";
+import Spinner from "@/src/components/ui/Spinner";
+
+const Page = () => {
+  const router = useRouter();
+  const { data: user, isLoading } = useLoadUserQuery(undefined, {});
+
+  useEffect(() => {
+    if (!isLoading && user?.user?.provider !== "manual") {
+      router.replace("/"); // client-side safe redirect
+    }
+  }, [isLoading, user, router]);
+
+  if (isLoading) return <Spinner />;
+
+  if (!user?.user || user.user.provider !== "manual") return null; // prevents UI flash
+
   return (
     <div className="w-full">
       <ChangePassword />
@@ -9,4 +27,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Page;
