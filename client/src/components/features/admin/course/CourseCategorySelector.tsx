e18eb2d13183b76fbcaa80e56ce3e480095896af
Ehtesham-Zahid <shamimalick321@gmail.com>
2025-10-17 12:16:39 +0500

    Categories Selector Updated

--- client/src/components/features/admin/course/CourseCategorySelector.tsx
+++ client/src/components/features/admin/course/CourseCategorySelector.tsx
@@ -6,6 +6,8 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/src/shadcn/ui/select";
+import { useGetCategoriesLayoutQuery } from "@/src/redux/features/layout/layoutApi";
+import Spinner from "@/src/components/ui/Spinner";
 
 type CourseCategorySelectorProps = {
   currentCategory: string;
@@ -16,34 +18,36 @@ const CourseCategorySelector = ({
   currentCategory,
   setCurrentCategory,
 }: CourseCategorySelectorProps) => {
-  return (
-    <Select value={currentCategory} onValueChange={setCurrentCategory}>
-      <SelectTrigger className="w-full dark:border-text2-dark border-text2 dark:text-text1-dark text-text1 py-5">
-        <SelectValue placeholder="Select a Category" />
-      </SelectTrigger>
-      <SelectContent className="dark:bg-background-dark bg-background">
-        <SelectGroup>
-          <SelectItem
-            value="web-development"
-            className="dark:hover:bg-surface-dark hover:bg-surface  "
-          >
-            Web Development
-          </SelectItem>
-          <SelectItem
-            value="mobile-development"
-            className="dark:hover:bg-surface-dark hover:bg-surface  "
-          >
-            Mobile Development
-          </SelectItem>
-          <SelectItem
-            value="ai-development"
-            className="dark:hover:bg-surface-dark hover:bg-surface  "
-          >
-            AI Development
-          </SelectItem>
-        </SelectGroup>
-      </SelectContent>
-    </Select>
+  const { data: categoriesData, isLoading: categoriesLoading } =
+    useGetCategoriesLayoutQuery();
+
+  const categories = categoriesData?.layout?.categories;
+
+  console.log("categories", categories);
+
+  return categoriesLoading ? (
+    <Spinner />
+  ) : (
+    <>
+      <Select value={currentCategory} onValueChange={setCurrentCategory}>
+        <SelectTrigger className="w-full dark:border-text2-dark border-text2 dark:text-text1-dark text-text1 py-5">
+          <SelectValue placeholder="Select a Category" />
+        </SelectTrigger>
+        <SelectContent className="dark:bg-background-dark bg-background">
+          <SelectGroup>
+            {categories.map((category: any) => (
+              <SelectItem
+                key={category._id}
+                value={category.title}
+                className="dark:hover:bg-surface-dark hover:bg-surface  "
+              >
+                {category.title}
+              </SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    </>
   );
 };
 
