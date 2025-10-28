34ae7da42f151d71bc2ace28c9fee3d25e7904f8
Ehtesham-Zahid <shamimalick321@gmail.com>
2025-10-28 19:02:44 +0500

    Email Fixed

--- server/utils/email.ts
+++ server/utils/email.ts
@@ -152,8 +152,9 @@ export const sendMail = async (options: EmailOptions) => {
   try {
     const transporter: Transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST,
-      port: parseInt(process.env.SMTP_PORT || "587"),
-      service: process.env.SMTP_SERVICE,
+      port: parseInt(process.env.SMTP_PORT || "465"),
+      secure: true,
+      // service: process.env.SMTP_SERVICE,
       auth: {
         user: process.env.SMTP_MAIL,
         pass: process.env.SMTP_PASS,
